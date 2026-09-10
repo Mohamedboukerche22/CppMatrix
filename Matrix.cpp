a0c358f492ec9c561a85a002691fe49bf3650e0d
Mohamed Boukerche <mohamedboukerche55@gmail.com>
2026-09-10 18:45:26 +0100

    performance huge boost

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,4 +1,7 @@
 #include<bits/stdc++.h>
+#include <immintrin.h>
+// compile g++ neural_net.cpp -O3 -mavx2 main.cpp -o net && ./net
+
 using namespace std;
 using Matrix=vector<vector<float>>;
 using vec=vector<float>;
@@ -18,10 +21,19 @@ void print(Matrix a){
 float sum(Matrix a){
   float Sum=0.0;
    for (size_t i = 0; i < a.size(); i++) {
-     for (size_t j = 0; j < a[0].size() ; j++) {
-         Sum+=a[i][j];
+     size_t j=0;
+     __m256 acc = _mm256_setzero_ps();
+     for (; j + 8 <= a[0].size(); j += 8){
+       __m256 va = _mm256_loadu_ps(&a[i][j]);
+       acc = _mm256_add_ps(acc, va);
      }
-     
+     __m128 lo = _mm256_castps256_ps128(acc);
+     __m128 hi = _mm256_extractf128_ps(acc, 1);
+     lo = _mm_add_ps(lo, hi);
+     lo = _mm_hadd_ps(lo, lo);
+     lo = _mm_hadd_ps(lo, lo);
+     Sum += _mm_cvtss_f32(lo);
+     for (; j < a[0].size(); j++) Sum += a[i][j];
    }
    return Sum;
 }
@@ -38,7 +50,7 @@ Matrix fill_matrix(size_t r, size_t c, float v) {
     return Matrix(r, vec(c,v));
 }
 
-Matrix operator+(const Matrix& a , const Matrix& b){
+/*Matrix operator+(const Matrix& a , const Matrix& b){
   if(a.empty()||b.empty()||a.size()!=b.size()||a[0].size()!=b[0].size()){
     //throw ereur "invalid shape";
     throw invalid_argument("Matrix dimensions don't match");
@@ -53,40 +65,51 @@ Matrix operator+(const Matrix& a , const Matrix& b){
     }
 
   return C;
-}
-Matrix operator-(const Matrix& a , const Matrix& b){
-  if(a.empty()||b.empty()||a.size()!=b.size()||a[0].size()!=b[0].size()){
-    //throw ereur "invalid shape";
-    throw invalid_argument("Matrix dimensions don't match");
-
-    
-  } 
-    Matrix C(a.size(),vec(a[0].size()));
-    for (size_t i = 0; i < a.size(); i++) {
-      for (size_t j = 0; j < a[0].size(); j++) {
-         C[i][j]=a[i][j]-b[i][j];
-      }
-    }
-
-  return C;
-}
-// useless function let use it to calc matmul
-
-/*Matrix operator*(const Matrix& a , const Matrix& b){
-  if(a.empty()||b.empty()||a.size()!=b.size()||a[0].size()!=b[0].size()){
-    //throw ereur "invalid shape";
-    throw invalid_argument("Matrix dimensions don't match");
-    
-  } 
-    Matrix C(a.size(),vec(a[0].size()));
-    for (size_t i = 0; i < a.size(); i++) {
-      for (size_t j = 0; j < a[0].size(); j++) {
-         C[i][j]=a[i][j]*b[i][j];
-      }
-    }
-
-  return C;
 }*/
+
+// -------------------- boost operator -------------------------------------------//
+Matrix operator-(const Matrix& a , const Matrix& b){
+    Matrix C(a.size(), vec(a[0].size()));
+    for (size_t i = 0; i < a.size(); i++){
+        size_t j = 0;
+        for (; j + 8 <= a[0].size(); j += 8){
+            __m256 va = _mm256_loadu_ps(&a[i][j]);
+            __m256 vb = _mm256_loadu_ps(&b[i][j]);
+            _mm256_storeu_ps(&C[i][j], _mm256_sub_ps(va, vb));
+        }
+        for (; j < a[0].size(); j++) C[i][j] = a[i][j] - b[i][j];
+    }
+    return C;
+}
+
+Matrix operator/(const Matrix& a , const Matrix& b){
+    Matrix C(a.size(), vec(a[0].size()));
+    for (size_t i = 0; i < a.size(); i++){
+        size_t j = 0;
+        for (; j + 8 <= a[0].size(); j += 8){
+            __m256 va = _mm256_loadu_ps(&a[i][j]);
+            __m256 vb = _mm256_loadu_ps(&b[i][j]);
+            _mm256_storeu_ps(&C[i][j], _mm256_div_ps(va, vb));
+        }
+        for (; j < a[0].size(); j++) C[i][j] = a[i][j] / b[i][j];
+    }
+    return C;
+}
+
+Matrix operator+(const Matrix& a, const Matrix& b){
+    Matrix C(a.size(), vec(a[0].size()));
+    for (size_t i = 0; i < a.size(); i++){
+        size_t j = 0;
+        for (; j + 8 <= a[0].size(); j += 8){
+            __m256 va = _mm256_loadu_ps(&a[i][j]);
+            __m256 vb = _mm256_loadu_ps(&b[i][j]);
+            __m256 vsum = _mm256_add_ps(va, vb);
+            _mm256_storeu_ps(&C[i][j], vsum);
+        }
+        for (; j < a[0].size(); j++) C[i][j] = a[i][j] + b[i][j];
+    }
+    return C;
+}
 
 Matrix operator*(const Matrix& A, const Matrix& B) {
     if (A.empty() || B.empty()) {
@@ -100,10 +123,22 @@ Matrix operator*(const Matrix& A, const Matrix& B) {
     Matrix C(A.size(), vec(B[0].size(), 0.0f));
 
     for (size_t i = 0; i < A.size(); ++i) {
-        for (size_t j = 0; j < B[0].size(); ++j) {
+        size_t j = 0;
+        for (; j + 8 <= B[0].size(); j += 8) {
+            __m256 acc = _mm256_setzero_ps();
             for (size_t k = 0; k < B.size(); ++k) {
-                C[i][j] += A[i][k] * B[k][j];
+                __m256 va = _mm256_set1_ps(A[i][k]);
+                __m256 vb = _mm256_loadu_ps(&B[k][j]);
+                acc = _mm256_add_ps(acc, _mm256_mul_ps(va, vb));
             }
+            _mm256_storeu_ps(&C[i][j], acc);
+        }
+        for (; j < B[0].size(); ++j) {
+            float s = 0.0f;
+            for (size_t k = 0; k < B.size(); ++k) {
+                s += A[i][k] * B[k][j];
+            }
+            C[i][j] = s;
         }
     }
 
@@ -111,42 +146,22 @@ Matrix operator*(const Matrix& A, const Matrix& B) {
 }
 
 Matrix operator*(const Matrix& A, float scalar){
-    if (A.empty()){
-        throw invalid_argument("Cannot multiply empty matrices");
-    }
-        Matrix C(A.size(), vec(A[0].size(), 0.0f));
-
+    Matrix C(A.size(), vec(A[0].size(), 0.0f));
+    __m256 vs = _mm256_set1_ps(scalar);
     for (size_t i = 0; i < A.size(); ++i) {
-        for (size_t j = 0; j < A[0].size(); ++j) {
-                C[i][j] = A[i][j] * scalar;
+        size_t j = 0;
+        for (; j + 8 <= A[0].size(); j += 8){
+            __m256 va = _mm256_loadu_ps(&A[i][j]);
+            _mm256_storeu_ps(&C[i][j], _mm256_mul_ps(va, vs));
         }
+        for (; j < A[0].size(); ++j) C[i][j] = A[i][j] * scalar;
     }
-
     return C;
-
-     
 }
 
 Matrix operator*(float scalar, const Matrix& A){
     return A * scalar;
 }
-
-Matrix operator/(const Matrix& a , const Matrix& b){
-  if(a.empty()||b.empty()||a.size()!=b.size()||a[0].size()!=b[0].size()){
-    //throw ereur "invalid shape";
-    throw invalid_argument("Matrix dimensions don't match");
-
-    
-  } 
-    Matrix C(a.size(),vec(a[0].size()));
-    for (size_t i = 0; i < a.size(); i++) {
-      for (size_t j = 0; j < a[0].size(); j++) {
-         C[i][j]=(a[i][j])/(b[i][j]);
-      }
-    }
-  return C;
-}
-
 
 Matrix transpose(const Matrix &a){
   Matrix T(a[0].size(),vec(a.size()));
@@ -175,18 +190,40 @@ Matrix fill_random(size_t rows, size_t cols, float min, float max) {
 }
 
 Matrix Hadamard(const Matrix &a,const Matrix &b){
-    if(a.empty()||b.empty()||a.size()!=b.size()||a[0].size()!=b[0].size()){
-    //throw ereur "invalid shape";
-    throw invalid_argument("Matrix dimensions don't match");
-    } 
-    Matrix C(a.size(),vec(a[0].size()));
+    Matrix C(a.size(), vec(a[0].size()));
     for (size_t i = 0; i < a.size(); i++) {
-      for (size_t j = 0; j < a[0].size(); j++) {
-         C[i][j]=(a[i][j])*(b[i][j]);
-      }
+        size_t j = 0;
+        for (; j + 8 <= a[0].size(); j += 8){
+            __m256 va = _mm256_loadu_ps(&a[i][j]);
+            __m256 vb = _mm256_loadu_ps(&b[i][j]);
+            _mm256_storeu_ps(&C[i][j], _mm256_mul_ps(va, vb));
+        }
+        for (; j < a[0].size(); j++) C[i][j] = a[i][j] * b[i][j];
     }
   return C;
+}
 
+Matrix pow_of(const Matrix& a , float n){
+    Matrix C(a.size(), vec(a[0].size()));
+
+    if (n == 2.0f) { 
+        for (size_t i = 0; i < a.size(); i++) {
+            size_t j = 0;
+            for (; j + 8 <= a[0].size(); j += 8){
+                __m256 va = _mm256_loadu_ps(&a[i][j]);
+                _mm256_storeu_ps(&C[i][j], _mm256_mul_ps(va, va));
+            }
+            for (; j < a[0].size(); j++) C[i][j] = a[i][j] * a[i][j];
+        }
+        return C;
+    }
+
+    for (size_t i = 0; i < a.size(); i++) {
+        for (size_t j = 0; j < a[0].size(); j++) {
+            C[i][j] = pow(a[i][j], n);
+        }
+    }
+    return C;
 }
 
 Matrix power(const Matrix& a , const Matrix& b){
@@ -205,16 +242,15 @@ Matrix power(const Matrix& a , const Matrix& b){
 
   return C;
 }
-Matrix pow_of(const Matrix& a , float n){
-    Matrix C(a.size(),vec(a[0].size()));
-    for (size_t i = 0; i < a.size(); i++) {
-      for (size_t j = 0; j < a[0].size(); j++) {
-         C[i][j]=pow(a[i][j],n);
-      }
-    }
-
-  return C;
+float avrg(const Matrix& a){
+  float Sum = sum(a);
+  float r = a.size(),c = a[0].size();
+  float shape = r*c;
+  return Sum/shape;
 }
+
+
+
 /*int main(){
       
   Matrix A = fill_random(5,5,0.f,0.5f);
