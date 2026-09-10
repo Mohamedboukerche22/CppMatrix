2857cbf04e30c0d5aa5ef484ed422e02923d3818
Mohamed Boukerche <mohamedboukerche55@gmail.com>
2026-09-10 18:06:48 +0100

    Update neural_net.cpp

--- neural_net.cpp
+++ neural_net.cpp
@@ -12,9 +12,12 @@ Matrix operator+(const Matrix& a , const Matrix& b);
 Matrix operator-(const Matrix& a , const Matrix& b);
 Matrix operator*(const Matrix& a , const Matrix& b);
 Matrix operator/(const Matrix& a , const Matrix& b);
+Matrix operator*(const Matrix& a, float scalar);
+
 
 Matrix fill_matrix(size_t r, size_t c, float v);
 Matrix power(const Matrix& a , const Matrix& b);
+Matrix pow_of(const Matrix& a , float n);
 Matrix transpose(const Matrix &a);
 
 
@@ -32,23 +35,21 @@ int main(){
   Matrix W=fill_random(5,5,-1.f,1.f);
   Matrix b=zeros(5,5);
   float lr=0.01f,n_sample = 5.f;
-  for(size_t epoch=0;epoch<7000;epoch++){
+  for(size_t epoch=0;epoch<500;epoch++){
     Matrix y_pred = X * W + b;
-    Matrix loss = power(y_pred-y,fill_matrix(y.size(),y[0].size(),2.f));
-    print(loss);
-    printf(" = epoche %d=\n",epoch);
-    Matrix dW = fill_matrix(y.size(),y[0].size(),(2/n_sample)) * (transpose(X) * (y_pred-y));
-    float db = (2/n_sample) * (sum(y_pred-y));
-    W = W - fill_matrix(5,5,lr)*dW;
-    b = b - fill_matrix(5,5,lr*db);
+    Matrix diff = y_pred - y;
+    if(epoch % 100 == 0){
+      printf("epoch %d | loss: %.6f\n", epoch, sum(pow_of(diff,2.0f))/25);
+    }
+    Matrix dW = (transpose(X) * (y_pred-y)) * (2.f/n_sample);
+    Matrix db = (y_pred-y) * (2.f/n_sample);
+    W = W - dW * lr;
+    b = b - db * lr;
   }
 
   //printf("w ==>");print(W);
   //printf("b ==>");print(b);
-  printf("ans = %.4f (expected %.4f)\n", X[0][0]*sum(W)/(5*5)+sum(b)/(5*5), X[0][0]+5);
+  printf("ans = %.4f (expected %.4f)\n", (X*W+b)[0][0], X[0][0]+5);
 
-
-  
   
 }
-
